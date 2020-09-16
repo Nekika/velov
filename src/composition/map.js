3dd91fdd1ac0656459a5660c3a7b1225e2c04c61
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-09-16 11:14:28 +0200

    Composition - Creating useCreateMap This hook creates a new map using Leaflet. From a stations list it will create markers, binding them a popup and add them to the map.

---
+++ src/composition/map.js
@@ -0,0 +1,43 @@
+import L from 'leaflet'
+import { reactive, onMounted } from 'vue'
+
+const LYON_LAT_LNG = [45.763420, 4.834277]
+const BASE_ZOOM = 13
+
+const createPopUpContent = properties => {
+    return `<h3>${properties.name}</h3>
+    <p><b>Address</b> : ${properties.address}</p>
+    <p><b>Available bikes</b> : ${properties.available_bikes}</p>
+    <p><b>Available bike stands</b> : ${properties.available_bike_stands}</p>
+    <p><b>Last update</b> : ${properties.last_update}</p>
+   `
+}
+    
+const createMarkers = (map, features) => {
+    return features.forEach(f => {
+        const coordinates = f.geometry.coordinates.reverse()
+        const popupContent = createPopUpContent(f.properties)
+        L.marker(coordinates)
+        .bindPopup(popupContent)
+        .addTo(map)
+    })
+}
+
+export const useCreateMap = stations => {
+
+    const state = reactive({
+        map: null
+    })
+
+    onMounted(() => {
+        state.map = L.map('map').setView(LYON_LAT_LNG, BASE_ZOOM)
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+                minZoom: 1,
+                maxZoom: 20
+            }).addTo(state.map)
+        createMarkers(state.map, stations)
+    })
+
+    return { state }
+}
