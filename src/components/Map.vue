833ac3ff31ed1fba9892aad8c0f0af8838e6ecb1
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-09-16 11:15:36 +0200

    Components - Creating Map The component from which the map will be created.

---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import { useCreateMap } from '../composition/map'
+
+export default {
+    props: {
+        stations: Array
+    },
+    setup(props) {
+        const { state } = useCreateMap(props.stations)
+        return { state }
+    }
+}
+</script>
+
+<style>
+#map {
+    height: 100%;
+    width: 100%;
+}
+</style>
